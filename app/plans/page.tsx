2bf9b2cd89b433e5ac36f0457248f8c0d8b813e2
hanhchoco.1581 <thuhanh6969@gmail.com>
2026-03-26 20:04:18 +0700

    Create page.tsx

---
+++ app/plans/page.tsx
@@ -0,0 +1,15 @@
+export default function Plans() {
+  return (
+    <div>
+      <h2>Plans</h2>
+
+      <p>My future learning roadmap.</p>
+
+      <ul>
+        <li>Learn HTML & CSS</li>
+        <li>Learn Testing</li>
+        <li>Build projects</li>
+      </ul>
+    </div>
+  );
+}
