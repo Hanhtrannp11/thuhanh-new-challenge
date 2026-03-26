e35adf6013bad971d8b25ffa2bef62d23af4f63c
hanhchoco.1581 <thuhanh6969@gmail.com>
2026-03-26 20:03:49 +0700

    Create page.tsx

---
+++ app/practices/page.tsx
@@ -0,0 +1,14 @@
+export default function Practices() {
+  return (
+    <div>
+      <h2>Practices</h2>
+
+      <p>Here I store all my exercises and small projects.</p>
+
+      <ul>
+        <li>HTML practice</li>
+        <li>Testing exercises</li>
+      </ul>
+    </div>
+  );
+}
