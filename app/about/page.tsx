f57737fdc90b52be4d499d1b9da379266676cdf7
hanhchoco.1581 <thuhanh6969@gmail.com>
2026-03-26 14:00:46 +0700

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>About</h2>
+
+      <p>
+        Hello, my name is Thu Hanh. I am starting my journey
+        from zero background.
+      </p>
+
+      <p>
+        This website is where I document my learning process,
+        challenges, and growth.
+      </p>
+    </div>
+  );
+}
